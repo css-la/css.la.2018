e2303d93b1aa2caa87f88431fe6fce13ff0fb1c3
laras126 <laras126@gmail.com>
2018-04-06 14:30:19 -0700

    Add initial content, update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,11 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Welcome to CSS.la!</h1>
+    <p>Los Angeles' premier meetup for CSS and front-end design enthusiasts.</p>
+    <p>Our first meetup is April 25, 2018 in Los Feliz!</p>
+    <a href="https://www.meetup.com/LearnTeachCode/events/249510027/">RSVP here</a>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </div>
 )
 
