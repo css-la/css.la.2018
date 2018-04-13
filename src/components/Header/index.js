6275e89ec55d488c49b6088f8547514eeb572b24
laras126 <laras126@gmail.com>
2018-04-13 15:11:39 -0700

    Add CoC file and placeholder link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,6 +6,7 @@ import './header.css'
 const Header = () => (
   <header className="header wrap">
     <span className="header__logo">CSS.la</span>
+    {/* <Link className="header__link" to="code-of-conduct">Code of Coduct</Link> */}
   </header>
 )
 
---
+++ src/pages/code-of-conduct.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import './pages.css';
+
+const IndexPage = () => (
+  <main>
+    <h1>Code of Conduct</h1>
+  </main>
+)
+
+export default IndexPage
