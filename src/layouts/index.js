0abc5594a897b6e7e500bd699dc164ec9af5a7ea
laras126 <laras126@gmail.com>
2018-04-13 08:30:42 -0700

    Update meta description and some copy

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,10 +8,10 @@ import './index.css'
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="CSS.la | California Style Sweets"
+      title="CSS.la | CSS Meetup in Los Angeles"
       meta={[
-        { name: 'description', content: 'CSS.la is Los Angeles\'s premiere CSS user group! Join us for all things CSS-related.' },
-        { name: 'keywords', content: 'css, los angeles, meetup, front-end' },
+        { name: 'description', content: 'CSS.la is Los Angeles\'s premiere CSS meetup! Join us for all things at the front of the front-end: UI development, design systems, accessibility, and of course, CSS.' },
+        { name: 'keywords', content: 'css, los angeles, meetup, front-end, html' },
       ]}
     />
     <Header />
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ import './pages.css';
 const IndexPage = () => (
   <main>
     <h1>Welcome to CSS.la!</h1>
-    <p>California Style Sweets: Los Angeles' premier meetup for CSS and front-end design enthusiasts.</p>
-    <p>Our first meetup is <strong>April 25, 2018 at 6:30pm</strong> in Los Feliz! The theme will be "CSS and Browser Mechanics". After a brief presentation from <a href="https://twitter.com/laras126">Lara</a>, we'll apply our newfound knowledge and work together on the CSS for this site (even though it's pretty amazing already 🤔).</p>
+    <p>Los Angeles' premier (and brand new!) meetup for CSS and front-end enthusiasts.</p>
+    <p>Our first meetup is <strong>April 25, 2018 at 6:30pm</strong> in Los Feliz, and the theme will be "CSS and Browser Mechanics". After a brief presentation from <a href="https://twitter.com/laras126">Lara</a>, we'll apply our newfound knowledge and work together on the CSS for this site (even though it's pretty amazing already 🤔).</p>
     <div className="spacer">
       <a className="button" href="https://www.meetup.com/LearnTeachCode/events/249510027/">RSVP on Meetup.com</a>
     </div>
