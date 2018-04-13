252976819cc6497d532787b3504bf95203557253
laras126 <laras126@gmail.com>
2018-04-13 08:31:24 -0700

    Copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <main>
     <h1>Welcome to CSS.la!</h1>
     <p>Los Angeles' premier (and brand new!) meetup for CSS and front-end enthusiasts.</p>
-    <p>Our first meetup is <strong>April 25, 2018 at 6:30pm</strong> in Los Feliz, and the theme will be "CSS and Browser Mechanics". After a brief presentation from <a href="https://twitter.com/laras126">Lara</a>, we'll apply our newfound knowledge and work together on the CSS for this site (even though it's pretty amazing already 🤔).</p>
+    <p>Our first meetup is <strong>April 25, 2018 at 6:30pm</strong> in Los Feliz, and the theme will be "CSS and Browser Mechanics". After a brief presentation from <a href="https://twitter.com/laras126">Lara</a>, we'll apply our newfound perspective and work together on the CSS for this site (even though it's pretty amazing already 🤔).</p>
     <div className="spacer">
       <a className="button" href="https://www.meetup.com/LearnTeachCode/events/249510027/">RSVP on Meetup.com</a>
     </div>
