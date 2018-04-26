00fc6bc2252826b38ac86fc9ab180f85074c92de
laras126 <laras126@gmail.com>
2018-04-26 11:57:46 -0700

    Update copy for May

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,10 @@ import './pages.css';
 const IndexPage = () => (
   <main>
     <h1>Welcome to CSS.la!</h1>
-    <p>Los Angeles' premier (and brand new!) meetup for CSS and front-end enthusiasts.</p>
-    <p>Our first meetup is <strong>April 25, 2018 at 6:30pm</strong> in Los Feliz, and the theme will be "CSS and Browser Mechanics". After a brief presentation from <a href="https://twitter.com/laras126">Lara</a>, we'll apply our newfound perspective and work together on the CSS for this site (even though it's pretty amazing already 🤔).</p>
+    <p>Los Angeles' premier (and brand new!) group for CSS and front-end enthusiasts. So far, CSS.la consists of a monthly study group through Learn Teach Code, a fantastic and supportive community of developers all over the Los Angeles area.</p>
+    <p>Our next study group is scheduled for <strong>May 23, 2018 at 6:30pm</strong> in Culver City, graciously hosted by <a href="https://www.replicated.com/">Replicated</a>. Our theme for May will be "The Algorithms of CSS"!</p>
     <div className="spacer">
-      <a className="button" href="https://www.meetup.com/LearnTeachCode/events/249510027/">RSVP on Meetup.com</a>
+      <a className="button" href="https://www.meetup.com/LearnTeachCode/events/250076452/">Read More and RSVP on Meetup.com</a>
     </div>
     <p>We'd like to get some information from you to help inform the events we plan. Help us out by filling out the super fancy form below:</p>
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfgzI3f4UC_WgCFd7yh3CQiINqjW-MYiHMlGGbnB4VJmXuTUQ/viewform?embedded=true" width="100%" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
