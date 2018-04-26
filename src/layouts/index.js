1cf81cb2ec61be5429d3b9e05deeef41396958a8
laras126 <laras126@gmail.com>
2018-04-26 11:58:42 -0700

    Change meta desc

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,7 +10,7 @@ const TemplateWrapper = ({ children }) => (
     <Helmet
       title="CSS.la | CSS Meetup in Los Angeles"
       meta={[
-        { name: 'description', content: 'CSS.la is Los Angeles\'s premiere CSS meetup! Join us for all things at the front of the front-end: UI development, design systems, accessibility, and of course, CSS.' },
+        { name: 'description', content: 'CSS.la is Los Angeles\'s premiere CSS group! Join us for all things at the front of the front-end: UI development, design systems, accessibility, and of course, CSS.' },
         { name: 'keywords', content: 'css, los angeles, meetup, front-end, html' },
       ]}
     />
