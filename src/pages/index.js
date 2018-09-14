4a4e263f6cc68e360d5d77c507171aba3caa7423
laras126 <laras126@gmail.com>
2018-09-13 21:24:01 -0700

    Update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,14 @@ import './pages.css';
 const IndexPage = () => (
   <main>
     <h1>Welcome to CSS.la 🌴</h1>
-    <div className="spacer">
-      <a className="button" href="https://paper.dropbox.com/doc/CSS.la-4-Naming-Conventions-in-CSS--AMm_gQrSk6NDxcdnsPpeSD0BAg-NNwsgZES3OpJAOINatkO3">Tonight's Notes</a>
-    </div>
     <p>CSS.la is Los Angeles' premier group for CSS and front-end enthusiasts. So far, CSS.la consists of a monthly-ish study group through <a href="https://www.meetup.com/LearnTeachCode/">Learn Teach Code</a>, a fantastic and supportive community of developers all over the Los Angeles area.</p>
-    <p>Our next meeting is <strong>September 12</strong>, graciously hosted by <a href="https://fender.com/">Fender Guitars</a> in Hollywood. Our topic this month will be naming conventions in CSS!</p>
+    <p>Our next meeting is <strong>October 10</strong>, graciously hosted by <a href="https://www.replicated.com/">Replicated</a> in Culver City!</p>
     <div className="spacer">
-      <a className="button" href = "https://www.meetup.com/LearnTeachCode/events/253679813/">Read More and RSVP on Meetup.com</a>
+      <a className="button" href= "https://www.meetup.com/LearnTeachCode/events/254676315/?isFirstPublish=true">Read More and RSVP on Meetup.com</a>
     </div>
     <p>Our previous meetings (the newer ones with notes!):</p>
     <ul>
+      <li><a href="https://www.meetup.com/LearnTeachCode/events/253679813/">Naming Conventions in CSS</a> @ Fender, September 12, 2018, <a href="https://paper.dropbox.com/doc/CSS.la-4-Naming-Conventions-in-CSS--AMm_gQrSk6NDxcdnsPpeSD0BAg-NNwsgZES3OpJAOINatkO3">Notes</a></li>
       <li><a href="https://www.meetup.com/LearnTeachCode/events/253492626/">CSS Animations!!</a> @ 8th Light, August 21, 2018, <a href="https://paper.dropbox.com/doc/CSS.la-3-CSS-Animations-YGMB8TFeAka4O5jsLhvD5">Notes</a></li>
       <li><a href="https://www.meetup.com/LearnTeachCode/events/250076452/">Learn About Algorithms in CSS</a> @ Replicated, May 23, 2018</li>
       <li><a href="https://www.meetup.com/LearnTeachCode/events/249510027/">Learn about CSS and Browser Mechanics</a> @ Open Listings, April 25, 2018</li>
